96f2605ee91ed6aa9c6bff746d68b952b8ddd89f
陈恺垣 <chenkaiyuan1993@gmail.com>
2015-04-13 19:38:21 +0800

    增加piwik

--- template/bs3-bj/js.php
+++ template/bs3-bj/js.php
@@ -15,4 +15,19 @@ $view_marquee_msg = file_get_contents($OJ_SAE ? "saestor://web/msg.txt" : "./adm
     });
 
 </script>
+<!-- Piwik -->
+<script type="text/javascript">
+    var _paq = _paq || [];
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        var u="//172.16.11.199/piwik/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', 1]);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+</script>
+<noscript><p><img src="//172.16.11.199/piwik/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
 
